e9cb6f60c607c20f2d1d76a0c38c89e27fbe03fc
Rohit Bhandari <rsbhandari6283@gmail.com>
2026-04-17 15:33:54 +0530

    adding app.js

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+
+const mongoose = require("mongoose");
+
+const namesArr = ["Rohit", "Sanjeev", "Nitish"];
+
+mongoose.connect("mongodb://127.0.0.1:27017/internPractice").then(() => {
+  console.log("database connected");
+});
+
+app.get("/", (req, res) => {
+  res.send("welcome to Node.js Server");
+});
+
+app.get("/about", (req, res) => {
+  res.send("Rohit Bhandari, MCA");
+});
+
+app.get("/users", (req, res) => {
+
+  res.send(namesArr);
+});
+
+app.listen(3000, () => {
+  console.log("Listening at pot 3000");
+});
